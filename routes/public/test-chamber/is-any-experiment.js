844d239bc98e97677b26d3fcf935d739e6f24da5
Samanta <koushik.engine@gmail.com>
2023-02-23 17:25:49 +0530

    update

--- routes/public/test-chamber/is-any-experiment.js
+++ routes/public/test-chamber/is-any-experiment.js
@@ -4,10 +4,10 @@ const isAnyExperimentRouter = express.Router();
 const checkAccess = require("../../../middleware/checkAccess/checkAccess");
 const { TestChamber } = require("../../../models/schema");
 
-
 isAnyExperimentRouter.get("/", checkAccess, async (req, res) => {
   try {
-    console.log(req.assignedChamberId);
+    //console.log(req.assignedChamberId);
+    const currentDate = new Date();
     const result = await TestChamber.aggregate([
       { $match: { _id: mongoose.Types.ObjectId(req.assignedChamberId) } },
       { $unwind: "$testsPerformed" },
@@ -17,21 +17,72 @@ isAnyExperimentRouter.get("/", checkAccess, async (req, res) => {
         },
       },
       {
-        $sort: {
-          "testsPerformed.testScheduleDate": 1,
-        },
-      },
-      {
         $group: {
-          _id: "$_id",
+          _id: "$testsPerformed._id",
           testConfig: {
             $first: "$testsPerformed.testConfig",
           },
+          testScheduleDate: {
+            $first: "$testsPerformed.testScheduleDate",
+          },
+        },
+      },
+      {
+        $match: { testScheduleDate: { $lte: currentDate } },
+      },
+      {
+        $sort: {
+          testScheduleDate: 1,
         },
       },
     ]);
-    const testConfig = result[0]?.testConfig;
-    res.json(testConfig)
+    const testConfig = result[0].testConfig; //tests are exported as que first schedule first out
+
+    if (testConfig) {
+      const testConfigOut = [];
+      for (let channel of testConfig.channels) {
+        const channelInfoDefault = {
+          channelNumber: null,
+          testId: null,
+          overallMultiplier: null,
+          isConAmTe: null,
+          ambTemp: null,
+          noOfSubExp: null,
+        };
+
+        const channelInfo = {
+          ...channelInfoDefault,
+          channelNumber: channel.channelNumber,
+          testId: `${testConfig._id.toString()}_${channel.channelNumber}`,
+          overallMultiplier: channel.overallRowMultiplier,
+          isConAmTe: testConfig.isConAmTe,
+          ambTemp: testConfig.ambTemp,
+          noOfSubExp: channel.testFormats.length,
+        };
+        const steps = [];
+        for (let testFormat of channel.testFormats){
+          const step = {};
+          switch(testFormat.value){
+            case 1:
+              //Do this for this long
+              break;
+            case 2:
+              //Do this until this happen
+              break;
+            case 3:
+              //Run X
+              break;
+          }
+          steps.push(step)
+        }
+
+        steps.push(channelInfo);
+        testConfigOut.push(steps);
+      }
+      res.json(testConfigOut);
+    } else {
+      res.json(null);
+    }
   } catch (err) {
     console.log(err);
     res.status(500).json({ msg: "Error" });
