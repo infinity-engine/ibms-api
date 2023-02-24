1eb2c9f69852619b148b0cc66665483365f24ba6
Koushik Samanta <koushik.engine@gmail.com>
2023-02-24 08:29:53 +0530

    fix

---
+++ routes/public/test-chamber/TestConfigOut.ts
@@ -0,0 +1,59 @@
+interface TestConfigOut {
+  channels: [ChannelInfo];
+}
+interface ChannelInfo {
+  steps: [Step | DriveCycle];
+  info: {
+    testId: string;
+    overallMultiplier: number;
+    isConAmTe: boolean;
+    ambTemp: number;
+    noOfSubExp: number;
+  };
+}
+interface Step {
+  mode: number | null;
+  currentRate: number | null;
+  resVal: number | null;
+  powVal: number | null;
+  timeLimit: number | null;
+  multiplier: number | null;
+  ambTemp: number | null;
+  holdVolt: number | null;
+}
+interface DriveCycle {
+  timeStep: [number];
+  value: [number];
+  valueType: "Current" | "Power";
+}
+
+const myConfig = {
+  channels: [
+    {
+      steps: [
+        {
+          mode: 1,
+          currentRate: 0.5,
+          resVal: null,
+          powVal: null,
+          timeLimit: 60,
+          multiplier: 1,
+          ambTemp: null,
+          holdVolt: null,
+        },
+        {
+          timeStep: [0, 30, 60],
+          value: [0, 2, 0],
+          valueType: "Current",
+        },
+      ],
+      info: {
+        testId: "myTestId",
+        overallMultiplier: 2,
+        isConAmTe: true,
+        ambTemp: 25,
+        noOfSubExp: 3,
+      },
+    },
+  ],
+};
--- routes/public/test-chamber/is-any-experiment.js
+++ routes/public/test-chamber/is-any-experiment.js
@@ -8,7 +8,7 @@ isAnyExperimentRouter.get("/", checkAccess, async (req, res) => {
   try {
     //console.log(req.assignedChamberId);
     const currentDate = new Date();
-    const result = await TestChamber.aggregate([
+    TestChamber.aggregate([
       { $match: { _id: mongoose.Types.ObjectId(req.assignedChamberId) } },
       { $unwind: "$testsPerformed" },
       {
@@ -35,58 +35,106 @@ isAnyExperimentRouter.get("/", checkAccess, async (req, res) => {
           testScheduleDate: 1,
         },
       },
-    ]);
-    const testConfig = result[0].testConfig; //tests are exported as que first schedule first out
-
-    if (testConfig) {
-      const testConfigOut = [];
-      for (let channel of testConfig.channels) {
-        const channelInfoDefault = {
-          channelNumber: null,
-          testId: null,
-          overallMultiplier: null,
-          isConAmTe: null,
-          ambTemp: null,
-          noOfSubExp: null,
-        };
-
-        const channelInfo = {
-          ...channelInfoDefault,
-          channelNumber: channel.channelNumber,
-          testId: `${testConfig._id.toString()}_${channel.channelNumber}`,
-          overallMultiplier: channel.overallRowMultiplier,
-          isConAmTe: testConfig.isConAmTe,
-          ambTemp: testConfig.ambTemp,
-          noOfSubExp: channel.testFormats.length,
-        };
-        const steps = [];
-        for (let testFormat of channel.testFormats){
-          const step = {};
-          switch(testFormat.value){
-            case 1:
-              //Do this for this long
-              break;
-            case 2:
-              //Do this until this happen
-              break;
-            case 3:
-              //Run X
-              break;
-          }
-          steps.push(step)
-        }
-
-        steps.push(channelInfo);
-        testConfigOut.push(steps);
-      }
-      res.json(testConfigOut);
-    } else {
-      res.json(null);
-    }
+    ]).then((result) => {
+      const testConfig = result[0].testConfig;
+      //tests are exported as queue first schedule first out
+      res.json(testConfig);
+      console.log(getOutput(testConfig))
+      //getOutput(testConfig);
+    });
   } catch (err) {
     console.log(err);
     res.status(500).json({ msg: "Error" });
   }
 });
 
+async function getOutput(testConfig) {
+  if (testConfig) {
+    const testConfigOut = { channels: [] };
+    for (let channel of testConfig.channels) {
+      
+      const config = {};
+      const channelInfoDefault = {
+        channelNumber: null,
+        testId: null,
+        overallMultiplier: null,
+        isConAmTe: null,
+        ambTemp: null,
+        noOfSubExp: null,
+      };
+      const channelInfo = {
+        ...channelInfoDefault,
+        channelNumber: channel.channelNumber,
+        testId: `${testConfig._id.toString()}_${channel.channelNumber}`,
+        overallMultiplier: channel.overallRowMultiplier,
+        isConAmTe: testConfig.isConAmTe,
+        ambTemp: testConfig.ambTemp,
+        noOfSubExp: channel.testFormats.length,
+      };
+
+      const steps = [];
+      getSteps(channel.testFormats, steps);
+      config.steps = steps;
+      config.info = channelInfo;
+      testConfigOut.channels.push(config);
+    }
+    return testConfigOut;
+  } else {
+    return null;
+  }
+}
+
+function getSteps(testFormats, steps) {
+  for (let testFormat of testFormats) {
+    const step = {};
+    switch (testFormat.value) {
+      case 1:
+        //Do this for this long
+        getStep_1(testFormat, step);
+        break;
+      case 2:
+        //Do this until this happen
+        getStep_2(testFormat, step);
+        break;
+      case 3:
+        //Run X
+        getStep_3(testFormat, step);
+        break;
+    }
+    steps.push(step);
+  }
+}
+function getStep_1(testFormat, step) {
+  step.multiplier = testFormat.multiplier ? testFormat.multiplier : 1;
+  step.ambTemp = testFormat.ambTemp ? testFormat.ambTemp : null;
+  const fields = testFormat.children[0].fields;
+  let field = fields[0];
+  if (field.id == 1) {
+    if (field.value == "Charge") {
+      // insert the logic for unit conversion from C/W to A
+      step.mode = 1;
+      step.currentRate = fields[2].value;
+    } else if (field.value == "Discharge") {
+      step.mode = 2;
+      step.currentRate = fields[2].value;
+    } else if (field.value == "Hold") {
+      step.mode = 9;
+      step.holdVolt = fields[2].value;
+    } else if (field.value == "Rest") {
+      step.mode = 8;
+    }
+  }
+
+  field = fields[6];
+  let timeLimit = fields[5].value;
+  if (field.value == "hours.") {
+    step.timeLimit = timeLimit * 3600;
+  } else if (field.value == "minutes.") {
+    step.timeLimit = timeLimit * 60;
+  } else if (field.value == "seconds.") {
+    step.timeLimit = timeLimit;
+  }
+}
+function getStep_2(testFormat, step) {}
+function getStep_3(testFormat, step) {}
 module.exports = isAnyExperimentRouter;
